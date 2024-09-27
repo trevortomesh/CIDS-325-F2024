1d6e8aff6e0dcc12dde57ab79d27a8d320f801d9
trevortomesh <tmtomesh@hotmail.com>
2024-09-27 13:01:33 -0500

    cookie clicker!

---
+++ cookie.js
@@ -0,0 +1,15 @@
+// Grab the HTML elements
+const scoreDisplay = document.getElementById('score');
+const cookie = document.getElementById('cookie');
+let score = 0; //let is used to declare a variable that can change
+// add an event listener to the cookie
+cookie.addEventListener('click', function(){
+    score += 1; //increase score
+    scoreDisplay.textContent = score;
+    let message = "Your score is: " + score;
+    console.log(message);
+})
+
+//score += 1;
+
+
