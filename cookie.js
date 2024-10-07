f75e2661fcc0b036aa413be9d5b3860538065abb
trevortomesh <tmtomesh@hotmail.com>
2024-10-07 13:13:56 -0500

    explored let vs var

--- cookie.js
+++ cookie.js
@@ -1,13 +1,13 @@
-var globalVar = "I'm global!";
-let globalLet = "Me too!";
-console.log(window.globalVar);
-console.log(window.globalLet);
-//{   //this is a block -- we can use this to limit the
-    // scope of things!
-    //let x = 10;
-    //console.log(x);
-//}
-// var name = "alice";
+// var globalVar = "I'm global!";
+// let globalLet = "Me too!";
+// console.log(window.globalVar);
+// console.log(window.globalLet);
+// {   //this is a block -- we can use this to limit the
+//     // scope of things!
+//     let x = 10;
+//     console.log(x);
+// }
+//var name = "alice";
 //var name = "bob";
 //let name = "alice";
 //let name = "bob";
@@ -31,6 +31,11 @@ const scoreDisplay = document.getElementById('score');
 const cookie = document.getElementById('cookie');
 let score = 0; //let is used to declare a variable that can change
 // add an event listener to the cookie
+let i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
 cookie.addEventListener('click', function(){
     score += 1; //increase score
     scoreDisplay.textContent = score;
@@ -46,6 +51,15 @@ cookie.addEventListener('click', function(){
         //console.log(foo);
         //console.log(x);
     }
+    if(score > 10){
+        do{
+            score+=1;
+            console.log("Score: " + score);
+        }while(score < 20);
+
+    }
+
+
 
 })
 
