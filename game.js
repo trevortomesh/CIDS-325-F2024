cae58531a433906b55540e8f3b6cf47eeb65f1c9
trevortomesh <tmtomesh@hotmail.com>
2024-10-30 21:01:56 -0500

    Discussion about prototypes and classes

--- game.js
+++ game.js
@@ -4,20 +4,20 @@ const ranks = ['2','3','4','5','6','7','8', '9', '10',
 
 //console.log(ranks[0] + " of " + suits[0]);
 
-let card = {
-    rank: ranks[0],
-    suit: suits[0],
-    displayCard(){
-      return `${this.rank} of ${this.suit}`;
-    }
-};
+// let card = {
+//     rank: ranks[0],
+//     suit: suits[0],
+//     displayCard(){
+//       return `${this.rank} of ${this.suit}`;
+//     }
+// };
 
 //card.rank = ranks[11];
 //console.log(card.displayCard());
 let deck = [];
-for(let i = 0; i < 52; i++){
-    deck[i] = card;
-}
+// for(let i = 0; i < 52; i++){
+//     deck[i] = card;
+// }
 
 let index = 0;
 for(let i = 0; i < suits.length; i++){
@@ -35,6 +35,71 @@ for(let i = 0; i < suits.length; i++){
     }
 }
 
+
+
+let joker = {
+    rank: 'Joker',
+    suit: 'Hearts',
+    displayCard(){
+        return `${this.rank} of ${this.suit}`; //template literal
+    }
+}
+
+
+deck.pop();
+deck.pop();
+deck.push(joker);
 for( let i=0; i < deck.length; i++){
     console.log(deck[i].displayCard());
 }
+
+// most objects have a prototype
+let empty = {};
+console.log(empty.toString);
+
+//console.log(Object.getPrototypeOf(empty) == Object.prototype);
+
+// console.log(Object.getPrototypeOf(Math) == Object.prototype);
+// console.log(Object.getPrototypeOf(Math.max) == Object.prototype);
+//
+// console.log(Object.getPrototypeOf([]) == Array.prototype);
+
+// let protoRabbit = {
+//     speak(line){
+//         console.log(`The ${this.type} rabbit says '${line}'`);
+//     }
+// };
+//
+// let killerRabbit = Object.create(protoRabbit);
+// killerRabbit.type = "killer";
+// killerRabbit.speak("SKREEEEEEEEEE!!!!");
+//
+// function makeRabbit(type){
+//     let rabbit = Object.create(protoRabbit);
+//     rabbit.type = type;
+//     return rabbit;
+// }
+//
+// function Rabbit(type){
+//     this.type = type;
+// }
+//
+// Rabbit.prototype.speak = function(line){
+//     console.log(`The ${this.type} rabbit says '${line}'`);
+//
+// };
+//
+// let weirdRabbit = new Rabbit("weird");
+// weirdRabbit.speak("I'm a sexy rabbit!");
+
+class Rabbit{
+    constructor(type) {
+        this.type = type;
+    }
+    speak(line){
+        console.log(`The ${this.type} rabbit says '${line}'`);
+    }
+}
+
+let blackRabbit = new Rabbit("Black");
+blackRabbit.speak("Meow!");
