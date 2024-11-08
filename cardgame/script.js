628622bd1a0b7d09acfd26721b0c70f1edd33510
trevortomesh <tmtomesh@hotmail.com>
2024-11-08 13:00:18 -0600

    working on the BlackJackGame class.

--- cardgame/script.js
+++ cardgame/script.js
@@ -25,21 +25,26 @@ class Card{
 
 
 class Deck{
-    constructor() {
+    constructor(numDecks = 4) {
         this.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
         this.ranks = ['2','3','4','5','6','7','8','9','10',
             'Jack', 'Queen', 'King', 'Ace'];
 
+        this.numDecks = numDecks;
         this.deck = [];
         this.initializeDeck();
     }
 
-    initializeDeck(){
-        for(let suit of this.suits){
-            for(let rank of this.ranks){
-                this.deck.push(new Card(rank, suit));
+    initializeDeck() {
+        this.deck = [];
+        for (let i = 0; i < this.numDecks; i++) {
+            for (let suit of this.suits) {
+                for (let rank of this.ranks) {
+                    this.deck.push(new Card(rank, suit));
+                }
             }
         }
+        this.shuffle();
     }
     shuffle(){
         for(let i = this.deck.length -1; i >0; i--){
@@ -49,38 +54,96 @@ class Deck{
         }
     }
 
+    deal(){
+        if(this.deck.length === 0){
+            this.initializeDeck();
+            alert("Deck reshuffled!");
+        }
+        return this.deck.pop();
+    }
+
     getCards(){
         return this.deck;
     }
 }
-function displayDeck(deck) {
-    const deckContainer = document.getElementById('deckContainer');
-    deckContainer.innerHTML = ' ';
 
-    for (let card of deck) {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
-        cardElement.style.backgroundImage =
-            `url(${card.getImagePath()})`;
-        deckContainer.appendChild(cardElement);
+class BlackJackGame{
+    constructor() {
+        //fill in later
     }
 
+    setupEventListeners(){
+        document.getElementById('dealButton').addEventListener('click', () =>
+            this.startGame());
+
+        document.getElementById('hitButton').addEventListener('click', () =>
+            this.playerHit());
+
+        document.getElementById('standButton').addEventListener('click', () =>
+            this.playerStand());
+    }
+
+    startGame(){
+        this.playerCardsDisplayed = 0;
+        this.dealerCardsDisplayed = 0;
+        document.getElementById('playerHand').innerHTML = ' ';
+        document.getElementById('dealerHand').innerHTML = ' ';
+        this.showMessage(" ");
+
+        document.getElementById('dealButton').disabled = true;
+        document.getElementById('hitButton').disabled = false;
+        document.getElementById('standButton').disable = false;
+
+        this.isGameActive = true;
+        this.hideDealerCard = true;
+        this.playerHand = [this.deck.deal(), this.deck.deal()];
+        this.dealerHand = [this.deck.deal(), this.deck.deal()];
+
+        this.updateDisplay();
+    }
+
+    playerHit(){
+        // fill in later
+    }
+
+    playerStand(){
+        // fill in later
+    }
+
+    calculateScore(hand){
+        // fill in later
+    }
 }
 
-const deck = new Deck();
-displayDeck(deck.getCards());
 
-document.getElementById('shuffleButton').addEventListener('click',() => {
-    deck.shuffle();
-    displayDeck(deck.getCards());
+// function displayDeck(deck) {
+//     const deckContainer = document.getElementById('deckContainer');
+//     deckContainer.innerHTML = ' ';
+//
+//     for (let card of deck) {
+//         const cardElement = document.createElement('div');
+//         cardElement.classList.add('card');
+//         cardElement.style.backgroundImage =
+//             `url(${card.getImagePath()})`;
+//         deckContainer.appendChild(cardElement);
+//     }
+//
+// }
 
-});
-
-
-myCard = new Card('Jack','Diamonds');
-console.log(myCard);
-
-window.onload = function() {
-    // Your JavaScript code here
-    alert(myCard.getValue());
-};
+// const deck = new Deck();
+// displayDeck(deck.getCards());
+//
+// document.getElementById('shuffleButton').addEventListener('click',() => {
+//     deck.shuffle();
+//     displayDeck(deck.getCards());
+//
+// });
+//
+//
+// myCard = new Card('Jack','Diamonds');
+// console.log(myCard);
+//
+// window.onload = function() {
+//     // Your JavaScript code here
+//     alert(myCard.getValue());
+// };
