e349c7725216e37f3fab06555304ed14339294fe
trevortomesh <tmtomesh@hotmail.com>
2024-12-04 15:36:35 -0600

    json and routes

--- activity/index.js
+++ activity/index.js
@@ -1,4 +1,13 @@
+const express = require('express');
 const fs = require('fs');
+
+const app = express();
+
+const PORT = 3000;
+
+// middleware to parse JSON data in request bodies
+app.use(express.json());
+
 
 function readJsonFile(filename){
     try{
@@ -19,13 +28,54 @@ function writeJsonFile(filename, data){
     }
 }
 
-const filename = 'data.json';
-let data = readJsonFile(filename);
-console.log('Current Data:', data);
+//get users by id
+app.get('/users/:id', (req, res) => {
+    const users = readJsonFile('data.json');
 
-const newEntry = {name: 'Diana', age: 28};
-data.push(newEntry);
+    const user = users.find(u => u.id === parseInt(req.params.id));
 
-writeJsonFile(filename, data); //write to json
+    //console.log(user);
+    if(user){
+        res.json(user);
+    }else{
+        res.status(404).json({message: 'User not found'});
+    }
+});
 
-console.log('Updated Data:', readJsonFile(filename));
+app.get('/users', (req,res) =>{
+    const users = readJsonFile('data.json');
+    res.json(users);
+});
+
+app.post('/users',(req,res) =>{
+    const users = readJsonFile('data.json');
+    const newUser = {id: Date.now(), ...req.body};
+    users.push(newUser);
+    writeJsonFile('data.json', users);
+    res.status(201).json(newUser);
+});
+
+
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+// const filename = 'data.json';
+// let data = readJsonFile(filename);
+// console.log('Current Data:', data);
+//
+// let dude = {};
+// dude.name = 'Guy';
+// dude.age = 27;
+//
+// const newEntry = {name: 'Diana', age: 28};
+// data.push(newEntry);
+// data.push(dude);
+//
+// writeJsonFile(filename, data); //write to json
+//
+// console.log('Updated Data:', readJsonFile(filename));
